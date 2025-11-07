ba8f9f9363f11e1e9767d9576e116a58c36a388f
AnvithaYalamanchili <anvithayalamanchili3@gmail.com>
2025-11-07 15:30:17 -0600

    Restricted to a single admin

--- app/crud.py
+++ app/crud.py
@@ -1,14 +1,22 @@
 from sqlalchemy.orm import Session
 from . import models,schemas
+from .models import RoleEnum
 from passlib.hash import argon2
+from fastapi import HTTPException as HTTP
 
 
 def create_user(db: Session, user: schemas.UserCreate):
     hashed_password = argon2.hash(user.password)
+    if user.role==RoleEnum.admin:
+        existing_admin=db.query(models.User).filter(models.User.role==RoleEnum.admin).first()
+        if existing_admin:
+            raise HTTP(status_code=400,detail="An admin user already exists.")
+
 
     db_user = models.User(
         name=user.name,
         email=user.email,
+        role=user.role,
         password_hash=hashed_password
     )
     db.add(db_user)
